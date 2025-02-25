a93feb2cfe3c10444fbdfd80c37140e7ad82de51
Bedirhan Deveci <bedirhanyigitdeveci@gmail.com>
2025-02-25 21:22:25 +0100

    Initial commit2

---
+++ src/test/java/com/example/obsapp/ObsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.obsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
